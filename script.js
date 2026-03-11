4b4f8acf2a378f388385315e6546a399359fdd18
hr1112-byte <hr.1112@icloud.com>
2026-03-12 02:00:33 +0900

    design update

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ document.querySelectorAll(".nav a").forEach(link => {
     nav.classList.remove("active");
   });
 });
+window.addEventListener("scroll", function () {
+  const header = document.querySelector(".header");
+
+  if (window.scrollY > 50) {
+    header.style.background = "rgba(5,5,10,0.98)";
+  } else {
+    header.style.background = "rgba(11,11,13,0.6)";
+  }
+});
